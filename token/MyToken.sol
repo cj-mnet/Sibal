a16561ed3b81174da40781dc1f414117bee3fc05
cj-mnet <79052852+cj-mnet@users.noreply.github.com>
2021-02-15 01:39:55 +0900

    Update MyToken.sol

--- token/MyToken.sol
+++ token/MyToken.sol
@@ -84,7 +84,7 @@ contract StandardToken is Token {
     uint256 public totalSupply;
 }
 
-contract SuperUltraPowerCoin is MCoin { // CHANGE THIS. Update the contract name.
+contract MCoin { // CHANGE THIS. Update the contract name.
 
     /* Public variables of the token */
 
@@ -104,7 +104,7 @@ contract SuperUltraPowerCoin is MCoin { // CHANGE THIS. Update the contract name
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function SuperUltraPowerCoin() {
+    function MCoin() {
         balances[msg.sender] = 100000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
         totalSupply = 1000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
         name = "MCoin";                                   // Set the name for display purposes (CHANGE THIS)
